53255cbc7539fc03bd26a723b82e27c38cb1f2ba
dhirajs0 <dhiraj.kumar2990@gmail.com>
2025-01-25 15:17:02 +0530

    balance constructor added

--- src/balances.rs
+++ src/balances.rs
@@ -8,3 +8,11 @@ pub struct Pallet {
     balances: BTreeMap<String, u128>
 }
 
+impl Pallet{
+    /// Create a new instance of the balances module.
+    pub fn new() -> Self{
+        Self {
+            balances: BTreeMap::new()
+        }
+    }
+}
